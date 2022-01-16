f50e73687908b1f9d77cbba5362b7dc8d69b4ba2
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2022-01-16 18:57:13 +0530

    Added solution - LeetHub

---
+++ Minimum Operations - GFG/minimum-operations.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+    def minOperation(self, n):
+        dp=[float("inf") for i in range(n+1)]
+        dp[0],dp[1]=0,1
+        for i in range(1,n+1):
+            if i+1<len(dp):
+                dp[i+1]=min(dp[i+1],dp[i]+1)
+            
+            if i*2<len(dp):
+                dp[i*2]=min(dp[i*2],dp[i]+1)
+        
+
+        return dp[-1]
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        ob = Solution()
+        print(ob.minOperation(n))
+# } Driver Code Ends

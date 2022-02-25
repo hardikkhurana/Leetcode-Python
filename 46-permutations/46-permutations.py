d993ad375327f1941664d6fd81d34cc05646a9bb
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2022-02-25 18:47:25 +0530

    Time: 39 ms (89.99%), Space: 14.2 MB (63.78%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,4 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        l=[]
+        def helper(a,n):
+            if n==1:
+                l.append(a.copy())
+                return
+            for i in range(n):
+                helper(a,n-1)
+                if n%2!=0:
+                    a[0],a[n-1]=a[n-1],a[0]
+                else:
+                    a[i],a[n-1]=a[n-1],a[i]
+                    
+        helper(nums,len(nums))
+        return l
+                
+
+
+"""
 import itertools
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         return itertools.permutations(nums)
+"""

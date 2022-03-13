92082a49f5a7b547508fe7eeaa6ab6781a0d2f22
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2022-03-13 16:17:42 +0530

    Added solution - LeetHub

---
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.py
@@ -0,0 +1,94 @@
+#User function Template for python3
+
+''' Node for linked list:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+'''
+class Solution:
+    #Function to add two numbers represented by linked list.
+    def addTwoLists(self, first, second):
+        def reverse(curr):
+            prev=None
+            while curr:
+                next=curr.next
+                curr.next=prev
+                prev=curr
+                curr=next
+            return prev
+            
+        f=reverse(first)
+        s=reverse(second)
+        
+        ans=Node(0)
+        curr=ans
+
+        carry=0
+        while f or s or carry:
+            temp=0
+            if f:
+                temp+=f.data
+                f=f.next
+            if s:
+                temp+=s.data
+                s=s.next
+            temp+=carry
+            carry=temp//10
+            ans.next=Node(temp%10)
+            ans=ans.next
+        
+        return reverse(curr.next)
+            
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+# Node Class
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def insert(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+# prints the elements of linked list starting with head
+def printList(n):
+    while n:
+        print(n.data,end=' ')
+        n = n.next
+    print()
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        
+        n = int(input())
+        arr1 = ( int(x) for x in input().split() )
+        LL1 = LinkedList()
+        for i in arr1:
+            LL1.insert(i)
+        
+        m = int(input())
+        arr2 = ( int(x) for x in input().split() )
+        LL2 = LinkedList()
+        for i in arr2:
+            LL2.insert(i)
+        
+        res = Solution().addTwoLists(LL1.head, LL2.head)
+        printList(res)
+# } Driver Code Ends

e252f7c283528e8f7b2e264bdc47187c190ac5e3
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2022-04-12 15:04:11 +0530

    Time: 35 ms (87.12%), Space: 13.9 MB (93.38%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.py
@@ -0,0 +1,21 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        m,n=len(board),len(board[0])
+        dp=[[0 for j in range(n)]for i in range(m)]
+        def valid(a,b):
+            return a>=0 and a<m and b>=0 and b<n
+        diri=[-1,-1,-1,0,0,+1,+1,+1]
+        dirj=[-1,0,+1,-1,+1,-1,0,+1]
+        for i in range(m):
+            for j in range(n):
+                s=0
+                for k in range(8):
+                    if valid(i+diri[k],j+dirj[k]) and board[i+diri[k]][j+dirj[k]]==1:
+                        s+=1
+                if (s==2 or s==3) and board[i][j]==1:
+                    dp[i][j]=1
+                elif s==3 and board[i][j]==0:
+                    dp[i][j]=1
+        for i in range(m):
+            board[i]=dp[i]
+        

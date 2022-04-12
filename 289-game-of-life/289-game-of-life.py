07910554ded66feee017cd57d2e7095437d10319
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2022-04-12 15:27:39 +0530

    Time: 53 ms (35.08%), Space: 14 MB (51.25%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -1,6 +1,36 @@
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
         m,n=len(board),len(board[0])
+        def valid(a,b):
+            return a>=0 and a<m and b>=0 and b<n
+        diri=[-1,-1,-1,0,0,+1,+1,+1]
+        dirj=[-1,0,+1,-1,+1,-1,0,+1]
+        for i in range(m):
+            for j in range(n):
+                s=0
+                for k in range(8):
+                    if valid(i+diri[k],j+dirj[k]) and (board[i+diri[k]][j+dirj[k]]==1 or board[i+diri[k]][j+dirj[k]]==-1):
+                        s+=1
+                if s<2 and board[i][j]==1:
+                    board[i][j]=-1
+                elif (s==2 or s==3) and board[i][j]==1:
+                    board[i][j]=1
+                elif s>3 and board[i][j]==1:
+                    board[i][j]=-1
+                elif s==3 and board[i][j]==0:
+                    board[i][j]=2
+        for i in range(m):
+            for j in range(n):
+                if board[i][j]==-1:
+                    board[i][j]=0
+                if board[i][j]==2:
+                    board[i][j]=1
+        
+        
+"""
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        m,n=len(board),len(board[0])
         dp=[[0 for j in range(n)]for i in range(m)]
         def valid(a,b):
             return a>=0 and a<m and b>=0 and b<n
@@ -18,4 +48,4 @@ class Solution:
                     dp[i][j]=1
         for i in range(m):
             board[i]=dp[i]
-        
+"""        

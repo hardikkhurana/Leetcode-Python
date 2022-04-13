796cbcb792dacb15a990765e6e1ecedaa48b5773
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2022-04-13 12:01:33 +0530

    Time: 36 ms (81.42%), Space: 13.8 MB (85.84%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -1,25 +1,25 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        mat=[[0 for j in range(n)] for i in range(n)]
+        mat=[[0 for i in range(n)] for j in range(n)]
         l,r,t,b=0,n-1,0,n-1
-        target=n*n
-        count=1
-        while l<=r :
+        val=1
+        while l<=r:
             for i in range(l,r+1):
-                mat[t][i]=count
-                count+=1
+                mat[t][i]=val
+                val+=1
             t+=1
             for i in range(t,b+1):
-                mat[i][r]=count
-                count+=1
+                mat[i][r]=val
+                val+=1
             r-=1
             for i in range(r,l-1,-1):
-                mat[b][i]=count
-                count+=1
+                mat[b][i]=val
+                val+=1
             b-=1
             for i in range(b,t-1,-1):
-                mat[i][l]=count
-                count+=1
+                mat[i][l]=val
+                val+=1
             l+=1
         return mat
+            
         

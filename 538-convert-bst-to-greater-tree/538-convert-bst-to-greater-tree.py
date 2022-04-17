a0b0ac8a4472e732a92732be5ea4ff1b5b327072
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2022-04-17 10:28:53 +0530

    Time: 81 ms (89.92%), Space: 16.8 MB (28.97%) - LeetHub

---
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
@@ -0,0 +1,14 @@
+class Solution:
+	def convertBST(self, root: TreeNode) -> TreeNode:
+		sum = 0
+		
+		def sol(root: TreeNode) -> TreeNode:
+			nonlocal sum
+			if root:
+				sol(root.right)
+				root.val += sum
+				sum = root.val
+				sol(root.left)
+			return root
+		
+		return sol(root)   

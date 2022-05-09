c5c43cc34e9425169205fcc35822ece3f5d242b4
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2022-05-10 00:12:04 +0530

    Time: 82 ms (90.18%), Space: 17.4 MB (54.88%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,5 +1,26 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        i=0
+        res=[]
+        while i<len(intervals) and intervals[i][0]<newInterval[0]:
+            res.append(intervals[i])
+            i+=1
+        if res and res[-1][0]<=newInterval[0]<=res[-1][1]:
+            res[-1][1]=max(res[-1][1],newInterval[1])
+        else:
+            res.append(newInterval)
+        for i in intervals[i:]:
+            if res[-1][0]<=i[0]<=res[-1][1]:
+                res[-1][1]=max(res[-1][1],i[1])
+            else:
+                res.append(i)
+        return res
+            
+    
+        
+"""
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         intervals.append(newInterval)
         intervals.sort()
         stack=[]
@@ -11,4 +32,4 @@ class Solution:
                 stack.append(i)
                 
         return stack
-        
+        """

813bdc5893ad0195692ff0cc5f9e470b62a29e5b
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2022-05-16 14:59:53 +0530

    Time: 99 ms (70.97%), Space: 15.8 MB (27.48%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,4 +1,32 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        return int(eval("".join([char if char != '/' else '//' for char in s if char != ' '])))
-                
+        
+        def update(sign,num,stack):
+            if sign=="+":
+                stack.append(num)
+            if sign=="-":
+                stack.append(-num)
+            if sign=="*":
+                stack.append(stack.pop()*num)
+            if sign=="/":
+                stack.append(int(stack.pop()/num))
+            return stack
+        
+        def solve(i,s):
+            stack,num,sign=[],0,"+"
+            while i<len(s):
+                if s[i].isdigit():
+                    num=num*10 + int(s[i])
+                elif s[i] in ["+","-","/","*"]:
+                    stack=update(sign,num,stack)
+                    num,sign=0,s[i]
+                elif s[i]=="(":
+                    num,sign=solve(i+1,s)
+                elif s[i]==")":
+                    stack=update(sign,num,stack)
+                    return sum(stack),i
+                i+=1
+            stack=update(sign,num,stack)
+            return sum(stack)
+        
+        return solve(0,s)

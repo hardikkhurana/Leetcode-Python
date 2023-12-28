43cce5bb17ba5574720bd0ef32a061b2485323be
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2023-12-28 16:30:57 +0530

    Create file

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,9 @@
+[
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, n in enumerate(nums):
+            if target - n in d:
+                return [i, d[target - n]]
+            d[n] = i
+

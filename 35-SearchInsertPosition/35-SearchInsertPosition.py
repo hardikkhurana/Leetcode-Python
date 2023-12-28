45a181972f6ed39f53805c9611e0828300f1193f
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2023-12-28 16:39:36 +0530

    Create file

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -0,0 +1,12 @@
+[
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        
+        l,r=0,len(nums)
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid]<target:
+                l=mid+1
+            else:
+                r=mid
+        return l

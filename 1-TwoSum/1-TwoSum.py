92b0238c1c2248c79b974f25c78eb152a9c59825
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2024-03-07 19:05:25 +0530

    Update file 3/7/2024, 7:05:24 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,9 +1,8 @@
-[
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, n in enumerate(nums):
-            if target - n in d:
-                return [i, d[target - n]]
-            d[n] = i
-
+        s = {}
+        for i,e in enumerate(nums):
+            if target-e in s:
+                return [i,s[target-e]]
+            s[e]=i
+[2,7,11,15]

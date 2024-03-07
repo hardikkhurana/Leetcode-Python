73bd9cddfb07b606cab3fc3cc7500789639e7ac0
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2024-03-07 19:05:56 +0530

    Update file 3/7/2024, 7:05:55 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,13 +1,7 @@
-[
-import heapq
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        c=Counter(nums)
-        h = [[-j,i] for i,j in c.items()]
-        heapq.heapify(h)
-        res=[]
-        for _ in range(k):
-            j,i=heapq.heappop(h)
-            res.append(i)
-        return res
+        c = Counter(nums)
+        nums = [[j,i] for i,j in c.items()]
+        nums.sort(reverse = True)
+        return [j for i,j in nums][:k]
+[1,1,1,2,2,3]

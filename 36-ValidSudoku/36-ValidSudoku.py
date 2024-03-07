39c6a182d1485a33993945b0a404b44e94311824
Hardik Khurana <43683221+hardikkhurana@users.noreply.github.com>
2024-03-07 19:06:16 +0530

    Update file 3/7/2024, 7:06:15 PM

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,15 +1,23 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        z = 0
-        p = 1
-        for i,n in enumerate(nums):
-            if n==0:
-                z+=1
-            else:
-                p*=n
-        if z>1:
-            return [0]*len(nums)
-        if z==1:
-            return [0 if n!=0 else p for n in nums ]
-        return [p//n for n in nums]
-[1,2,3,4]
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        n,m = len(board), len(board[0])
+        rows = defaultdict(set)
+        col = defaultdict(set)
+        box = defaultdict(set)
+        
+        for i in range(n):
+            for j in range(m):
+                val = board[i][j]
+                if val == ".":
+                    continue
+                if val in rows[i]:
+                    return False
+                if val in col[j]:
+                    return False
+                if val in box[(i//3,j//3)]:
+                    return False
+                rows[i].add(val)
+                col[j].add(val)
+                box[(i//3,j//3)].add(val)
+        return True
+[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9",
